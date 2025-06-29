ffd9b75f371be80eb203b5c0c97bf687850a9c38
EC2 Default User <ec2-user@ip-172-31-81-188.ec2.internal>
2025-06-29 14:32:16 +0000

    Test new deployment version

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello Saed, from EC2 Dockerized Node.js App!');
+app.get("/", (req, res) => {
+  res.send("✅ New version deployed using GitHub Actions + Terraform!");
 });
 
 app.listen(port, '0.0.0.0', () => {
